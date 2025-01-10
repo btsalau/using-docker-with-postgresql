c4bd2e92cbd6a19307a75d4ca0a1d9f558876080
Bolakale Salau <94521694+btsalau@users.noreply.github.com>
2025-01-10 15:30:00 +0000

    testing

--- 3_workflow_orchestration/dags/local_ingest_dag.py
+++ 3_workflow_orchestration/dags/local_ingest_dag.py
@@ -5,3 +5,32 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+
+url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz"
+csv_file = "output.csv"
+
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+    "start_date": datetime(2024, 1, 1),
+}
+
+COMMON_ARGS = {
+    "dag_id": "data_ingestion",
+    "description": "An Airflow data ingestion script",
+    "schedule_interval": "@monthly",
+}
+
+# Combine arguments properly
+dag_args = {**COMMON_ARGS, "default_args": default_args}
+
+with DAG(**dag_args) as dag:
+    task1 = BashOperator(
+        task_id="wget", bash_command=f"wget -O - {url} | gunzip > {csv_file}"
+    )
+
+    task2 = BashOperator(task_id="check", bash_command="ls")
+
+    task1 >> task2
