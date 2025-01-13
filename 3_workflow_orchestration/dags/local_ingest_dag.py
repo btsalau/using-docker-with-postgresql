c6f8f9a620bf62116f2a55388e4b9e0c1059070d
Bolakale Salau <94521694+btsalau@users.noreply.github.com>
2025-01-13 17:46:24 +0000

    successfully added pythonoperator

--- 3_workflow_orchestration/dags/local_ingest_dag.py
+++ 3_workflow_orchestration/dags/local_ingest_dag.py
@@ -1,24 +1,43 @@
-""" Defining the URL
+""" Properly defining the URL for the ingestion script to run monthly
 URL_TEMPLATE = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
 version_date = datetime.now().strftime("%Y-%m")
 FULL_URL = URL_TEMPLATE + '/yellow_tripdata_' + version_date + '.csv.gz'
 """
 
+import os
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from sqlalchemy import create_engine
+
+
+def test_connection(DB_ENGINE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME):
+
+    try:
+        engine = create_engine(
+            f"{DB_ENGINE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        )
+        engine.connect()
+    except Exception as e:
+        return str(e)
+    return "Connection successful"
+
 
 # Load environment variables at the beginning of the script
 load_dotenv()
 
 # Access environment variables
-# engine = create_engine(f"{db_engine}://{user}:{password}@{host}:{port}/{db_name}")
-# db_engine, user, password, host, port, db_name = os.getenv("DB_ENGINE"), os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), os.getenv("DB_HOST"), os.getenv("DB_PORT"), os.getenv("DB_NAME")
-
-# print(db_engine, user, password, host, port, db_name)
-
+db_engine, user, password, host, port, db_name = (
+    os.getenv("DB_ENGINE"),
+    os.getenv("DB_USER"),
+    os.getenv("DB_PASSWORD"),
+    os.getenv("DB_HOST"),
+    os.getenv("DB_PORT"),
+    os.getenv("DB_NAME"),
+)
 
 URL = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz"
 
@@ -47,4 +66,17 @@ with DAG(**dag_args) as dag:
 
     task2 = BashOperator(task_id="files", bash_command="ls /opt/airflow/downloads")
 
-    task1 >> task2
+    task3 = PythonOperator(
+        task_id="engine_connection",  # task identifier
+        python_callable=test_connection,  # python function
+        op_kwargs={
+            "DB_ENGINE": db_engine,
+            "DB_USER": user,
+            "DB_PASSWORD": password,
+            "DB_HOST": host,
+            "DB_PORT": port,
+            "DB_NAME": db_name,
+        },  # function parameters and arguments
+    )
+
+    task1 >> task2 >> task3
